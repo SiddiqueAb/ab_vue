87d12bdcb33297cb3bf8d1f3cd171d7f82e9d7cf
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2023-04-01 12:51:54 +0600

    routing done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,12 @@
 <template>
   <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <router-link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32">
           <use xlink:href="#bootstrap" />
         </svg>
         <span class="fs-4">AB SIDDIQUE</span>
-      </a>
+      </router-link>
 
 
       <ul class="nav nav-pills">

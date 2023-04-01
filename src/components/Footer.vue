c7030a4fe10b2cdab6d6896ee87747d89348594b
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2023-04-01 19:05:26 +0600

    btn design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,18 @@
 <template>
     <footer class="container mt-5">
-        <p class="float-end"><a href="#">^</a></p>
-        <p>&copy; 2017–2021 ab@siddique, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p class="float-end"><a href="#" class="btn btn-primary text-light py-0">^</a></p>
+        <p>&copy; 2017–2021 ab@siddique, Inc. &middot; <a href="#" >Privacy</a> &middot; <a href="#">Terms</a></p>
     </footer>
 </template>
 <style>
 .float-end {
     /* list-style-type: none; */
+    
 }
 
 .float-end a {
     color: var(--bs-link-color);
     text-decoration: none;
+    font-size:20px;
 }
 </style>
